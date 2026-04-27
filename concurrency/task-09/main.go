4fb865991e713b1142c5b437285a315bb66832de
steepty <steeptyit@gmail.com>
2026-04-27 17:16:19 +0300

    Последние 2 задачи

--- concurrency/task-09/main.go
+++ concurrency/task-09/main.go
@@ -1,4 +1,4 @@
-//Вопросы:
+// Вопросы:
 // 1) Что будет если один из воркеров зависнет? Поможет ли тут закрытие канала?
 // 2) Нужен ли тут контекст, если да, то какой?
 // 3) Как поправить код, чтобы не бояться зависания воркеров ?
@@ -6,25 +6,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
 
-func worker(jobs <-chan int) {
-	for job := range jobs {
-		fmt.Println("processing", job)
-		time.Sleep(2 * time.Second)
+func worker(ctx context.Context, jobs <-chan int) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case job, ok := <-jobs:
+			if ok {
+				fmt.Println("processing", job)
+				time.Sleep(2 * time.Second)
+			} else {
+				return
+			}
+		}
 	}
 }
 
 func main() {
 	jobs := make(chan int)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	go worker(jobs)
+	go worker(ctx, jobs)
 
 	for i := 0; i < 5; i++ {
 		jobs <- i
 	}
+	close(jobs)
 
-	
 }
+
+// 1) Закрытие не поможет.
+// 2) Тут подойдет контекст с отменой. Чтобы в случае чего его можно было отменить.
+// 3) Как поправить код, чтобы не бояться зависания воркеров ?
+// 4) Где должен проверяться `<-ctx.Done()` ?
